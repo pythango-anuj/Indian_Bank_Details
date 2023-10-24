e321d8484e057d08b9fb2657d81b6a6b72d54b88
pythango-anuj <anujp.dev@gmail.com>
2023-10-24 22:17:12 +0530

    Added test case

--- Banks/settings.py
+++ Banks/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'indian_banks',
-        'USER': 'anuj',
-        'PASSWORD': 'anuj@postgres',
-        'HOST': '127.0.0.1',
+        'NAME': 'NAME',
+        'USER': 'USER',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'HOST',
         'PORT': '',
     }
 }
