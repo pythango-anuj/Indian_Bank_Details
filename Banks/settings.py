8a5324870fa672b9a71fa2f71608b2ae0e939af3
pythango-anuj <anujp.dev@gmail.com>
2023-10-24 23:55:46 +0530

    Updated

--- Banks/settings.py
+++ Banks/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!9c1-ilh#idqst@fs#v$9!(hq5iyx!swd=g86w3716lc&#6$wm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
